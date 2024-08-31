2ecabb7cdcd31685d1b0cfa362d793c54510d0cf
Amey Gaikar <gaikaramey04@gmail.com>
2024-08-31 23:12:52 +0530

    Add functionalitites to login page

--- server.js
+++ server.js
@@ -3,16 +3,41 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 
-
+const tokenAccessURL = "https://kitsu.io/api/oauth/token";
 const port = 3000;
 const app = express();
 
 app.use(express.static("./public"));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
-app.listen(port , () => {
-console.log(`listening on http://localhost:${port}`);
+app.get("/getStarted", (req, res) => {
+    res.render("account.ejs");
 })
+
+app.post("/submitKitsu",async (req, res) => {
+    const userPassword = req.body.password;
+    const userEmail = req.body.mail;
+try {
+        const response = await axios.post(tokenAccessURL, {
+        grant_type: 'password',
+        username: userEmail,
+        password: userPassword
+    })
+
+    console.log(response);
+} catch (error) {
+    console.error(error.message);
+}
+
+})
+
+
+app.listen(port, () => {
+    console.log(`listening on http://localhost:${port}`);
+})
+
