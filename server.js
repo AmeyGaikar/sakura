db590d3306238bc3aad222b352870d2d07532a09
Amey Gaikar <gaikaramey04@gmail.com>
2024-09-15 15:40:09 +0530

    add logic to prevent unauthorized access of homepage

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const tokenAccessURL = "https://kitsu.io/api/oauth/token";
 const allAnime = "https://kitsu.io/api/edge/anime"
 const port = 3000;
 const app = express();
+let userEmail;
+let userPassword;
+let token;
 
 app.use(express.static("./public"));
 
@@ -25,32 +28,36 @@ app.get("/getStarted", (req, res) => {
 app.post("/submitKitsu", async (req, res) => {
     const email = req.body.mail;
     const password = req.body.password;
+
+    userEmail = email;
+    userPassword = password;
     try {
-
-
         const response = await axios.post(tokenAccessURL, {
             grant_type: "password",
             username: email,
             password: password
         });
 
+        token = response.data.access_token;
         res.render("account.ejs", { token: response.data.access_token, error: false });
-        console.log(response);
     } catch (error) {
         console.error(error.response.data.error_description);
         res.render("account.ejs", { token: false, error: true });
     }
 })
 
-
 app.get("/homepage", async (req, res) => {
-    try {
-        const response = await axios.get(allAnime);
-        console.log(response.data.links.first);
-    } catch (error) {
-        console.error(error.response);
+    if ((userEmail && userPassword) || token) {
+        try {
+            const response = await axios.get(allAnime);
+            console.log(response.data.links.first);
+        } catch (error) {
+            console.error(error.response);
+        }
+        res.render("homepage.ejs");
+    } else {
+        res.send("<p>you're trying to access the homepage without logging in through kitsu, login using kitsu first.</p>");
     }
-    res.render("homepage.ejs");
 })
 
 app.listen(port, () => {
