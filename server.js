bfba5dd6f788179e683325f65e7cb5415b3e1872
Amey Gaikar <gaikaramey04@gmail.com>
2024-09-19 12:24:14 +0530

    Make next route work for trending anime

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 const homePageAnime = "https://kitsu.io/api/edge/anime?page%5Blimit%5D=18&page%5Boffset%5D=0";
 
-let homePageTrending = "https://kitsu.io/api/edge/anime?filter[categories]=trending&page%5Blimit%5D=18&page%5Boffset%5D=0";
+let trendingAnime = "https://kitsu.io/api/edge/anime?filter[categories]=trending&page%5Blimit%5D=18&page%5Boffset%5D=0";
 const tokenAccessURL = "https://kitsu.io/api/oauth/token";
 let allAnime = "https://kitsu.io/api/edge/anime?page[limit]=18&page[offset]=0";
 const port = 3000;
@@ -18,6 +18,10 @@ let userPassword;
 let nextLink;
 let prevLink;
 let lastLink;
+
+let trnextLink;
+let trprevLink;
+let trlastLink;
 app.use(express.static("./public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -62,7 +66,7 @@ app.get("/homepage", async (req, res) => {
     // if ((userEmail && userPassword) && token) {
     try {
         const response = await axios.get(homePageAnime);
-        const fetchTrending = await axios.get(homePageTrending);
+        const fetchTrending = await axios.get(trendingAnime);
 
         res.render("homepage.ejs", { animeData: response.data.data, trendingAnime: fetchTrending.data.data, homePageCheck: "on-homepage" });
 
@@ -81,20 +85,42 @@ app.get("/next", async (req, res) => {
 
     //here we are getting the response from allAnime varible.
     try {
-        const currentPage = await axios.get(allAnime);
 
-        //using the above response to get the next link from next links object 
-        nextLink = currentPage.data.links.next;
+        let trendingResponse, animeResponse;
+        if (req.query.pgtype == 'trending') {
+            console.log("test ok!");
 
-        //get the response from the next link .
-        const response = await axios.get(nextLink);
+            const currentPage = await axios.get(trendingAnime);
 
-        //setting the allAnime variable as next link. So that if the user clicks the next button again it will be redirected to the nextlink of the previously clicked which is now (current) link since we set the allAnime to have the value of nextLink. 
-        allAnime = nextLink;
+            trnextLink = currentPage.data.links.next;
 
-        const fetchTrending = await axios.get(homePageTrending);
+            trendingResponse = await axios.get(trnextLink);
+            trendingAnime = trnextLink;
 
-        res.render("homepage.ejs", { animeData: response.data.data, trendingAnime: fetchTrending.data.data});
+        } else {
+
+        
+            const currentPage = await axios.get(allAnime);
+
+            //using the above response to get the next link from next links object 
+            nextLink = currentPage.data.links.next;
+
+            //get the response from the next link .
+            animeResponse = await axios.get(nextLink);
+
+            //setting the allAnime variable as next link. So that if the user clicks the next button again it will be redirected to the nextlink of the previously clicked which is now (current) link since we set the allAnime to have the value of nextLink. 
+            allAnime = nextLink;
+        }
+
+        if(!trendingResponse) {
+            trendingResponse = await axios.get(trendingAnime);
+        }
+
+        if(!animeResponse) {
+            animeResponse = await axios.get(allAnime);
+        }
+
+        res.render("homepage.ejs", {animeData: animeResponse.data.data, trendingAnime: trendingResponse.data.data});
 
     } catch (error) {
         console.error(error.response);
@@ -105,24 +131,40 @@ app.get("/prev", async (req, res) => {
     //same logic as above but for previous link.
 
     try {
-        const currentPage = await axios.get(allAnime);
-        prevLink = currentPage.data.links.prev;
 
-        console.log(prevLink);
+        if (req.query.pgtype == 'trending') {
+            console.log("test ok!");
 
-        //here we are implementing logic that when we pressed previous button if we redirect to the homepage then we would want the home and the prevoious button to be hidden
+            const currentPage = await axios.get(trendingAnime);
 
-        //it says hide if homepage link is equals to the previous link.
-        let btnChk = String(homePageAnime) === String(prevLink) ? "hide" : "unhide";
+            trprevLink = currentPage.data.links.prev;
 
-        const response = await axios.get(prevLink);
-        allAnime = prevLink;
+            const response = await axios.get(trprevLink);
+            trendingAnime = trprevLink;
+
+            const fetchAllAnime = await axios.get(allAnime);
+            res.render("homepage.ejs", { trendingAnime: response.data.data, animeData: fetchAllAnime.data.data });
+
+        } else if (req.query.pgtype === undefined) {
+
+            const currentPage = await axios.get(allAnime);
+            prevLink = currentPage.data.links.prev;
+
+            console.log(prevLink);
+
+            //here we are implementing logic that when we pressed previous button if we redirect to the homepage then we would want the home and the prevoious button to be hidden
+
+            //it says hide if homepage link is equals to the previous link.
+            let btnChk = String(homePageAnime) === String(prevLink) ? "hide" : "unhide";
+
+            const response = await axios.get(prevLink);
+            allAnime = prevLink;
 
 
 
-        res.render("homepage.ejs", { animeData: response.data.data, isPrevHome: btnChk });
+            res.render("homepage.ejs", { animeData: response.data.data, isPrevHome: btnChk });
 
-
+        }
     } catch (error) {
         console.error(error.response);
     }
